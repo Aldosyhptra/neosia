3eba366af4c31f859613576a5a776bd79bf1b275
Aldo Syahputra <aldosyahputra80@gmail.com>
2025-08-10 01:21:54 +0700

    revisi hover dan margin

--- resources/views/components/landingpage/event.blade.php
+++ resources/views/components/landingpage/event.blade.php
@@ -1,69 +1,93 @@
 <section>
-    <div class="relative overflow-hidden py-10  bg-black">
+    <div class="relative overflow-hidden py-10 px-3 bg-black">
         <!-- Background kanan-->
         <div class="absolute inset-y-0 -right-15 w-1/3">
-            <img src="/img/landingpage/sidebg.png" 
-                class="h-full w-full object-cover transform" 
-                alt="">
+            <img src="/img/landingpage/sidebg.png" class="h-full w-full object-cover transform" alt="">
         </div>
 
         <!-- Konten di tengah -->
         <div class="max-w-7xl mx-auto relative z-10">
             <div class="flex justify-between items-start">
                 <h1 class="text-white font-semibold text-lg md:text-2xl">Public event</h1>
-                <a href="" class="bg-[#FFB71A] text-black py-2 px-7 rounded-md text-xs md:text-sm hover:bg-black hover:text-[#FFB71A] hover:outline hover:outline-[#FFB71A] transition-all">Lainnya ></a>
+                <a href=""
+                    class="bg-[#FFB71A] text-black py-2 px-7 rounded-md text-xs md:text-sm hover:bg-black hover:text-[#FFB71A] hover:outline hover:outline-[#FFB71A] transition-all">Lainnya
+                    ></a>
             </div>
             <div class="relative z-10 flex flex-col items-center justify-center h-full text-center px-3 my-10">
                 <div class="flex flex-wrap justify-center items-center gap-5">
-                    <div class="flex flex-col justify-evenly items-center rounded-md bg-white max-w-[50vh] group overflow-hidden" data-aos="fade-up">
+                    <div class="flex flex-col justify-evenly items-center rounded-md bg-white max-w-[50vh] group overflow-hidden"
+                        data-aos="fade-up">
                         <div class="overflow-hidden">
-                            <img src="/img/landingpage/event.png" alt="" class="rounded-t-md w-[50vh] h-[30vh] object-cover scale-105 overflow-hidden group-hover:scale-100 group-hover:brightness-90 transition-all">
+                            <img src="/img/landingpage/event.png" alt=""
+                                class="rounded-t-md w-[50vh] h-[30vh] object-cover scale-105 overflow-hidden group-hover:scale-100 group-hover:brightness-90 transition-all">
                         </div>
-                        <div class="flex flex-col gap-2 p-5">
+                        <div
+                            class="flex flex-col gap-2 p-5 group-hover:border-b-5 group-hover:border-x-5 rounded-b-md border-[#4F46E5] transition-all">
                             <p class="text-xs md:text-sm text-gray-800 text-start">20 Juni 2025 | Yogyakarta</p>
-                            <p class="font-semibold break-words text-start text-[#4F46E5] my-1">Open library talks  creative industry development.......</p>
-                            <p class="text-xs md:text-sm text-gray-800 text-start">In order to save time you have to break down the content strategy for the event or conference you are planning step by step..................</p>
+                            <p class="font-semibold break-words text-start text-[#4F46E5] my-1">Open library talks
+                                creative industry development.......</p>
+                            <p class="text-xs md:text-sm text-gray-800 text-start">In order to save time you have to
+                                break down the content strategy for the event or conference you are planning step by
+                                step..................</p>
                             <div class="flex gap-2 items-center mt-3">
-                                <a href="" class="px-5 py-2 rounded-2xl bg-[#FFB71A] text-white font-semibold text-sm hover:bg-white hover:text-[#FFB71A] hover:outline-[#FFB71A] hover:outline-1 transition-all">
+                                <a href=""
+                                    class="px-5 py-2 rounded-2xl bg-[#FFB71A] text-white font-semibold text-sm hover:bg-white hover:text-[#FFB71A] hover:outline-[#FFB71A] hover:outline-1 transition-all">
                                     Buy Tickets
                                 </a>
-                                <a href="" class="px-5 py-2 rounded-2xl bg-[#3D3D3D] text-white font-semibold text-sm hover:bg-gray-300 hover:text-black transition-all">
+                                <a href=""
+                                    class="px-5 py-2 rounded-2xl bg-[#3D3D3D] text-white font-semibold text-sm hover:bg-gray-300 hover:text-black transition-all">
                                     Details
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div class="flex flex-col justify-evenly items-center rounded-md bg-white max-w-[50vh] group overflow-hidden" data-aos="fade-up" data-aos-delay="50">
+                    <div class="flex flex-col justify-evenly items-center rounded-md bg-white max-w-[50vh] group overflow-hidden"
+                        data-aos="fade-up" data-aos-delay="50">
                         <div class="overflow-hidden">
-                            <img src="/img/landingpage/event.png" alt="" class="rounded-t-md w-[50vh] h-[30vh] object-cover scale-105 overflow-hidden group-hover:scale-100 group-hover:brightness-90 transition-all">
+                            <img src="/img/landingpage/event.png" alt=""
+                                class="rounded-t-md w-[50vh] h-[30vh] object-cover scale-105 overflow-hidden group-hover:scale-100 group-hover:brightness-90 transition-all">
                         </div>
-                        <div class="flex flex-col gap-2 p-5 border-b-2 border-x-2 group-hover:border-b-5 group-hover:border-x-5 rounded-b-md border-[#4F46E5] transition-all">
+                        <div
+                            class="flex flex-col gap-2 p-5 group-hover:border-b-5 group-hover:border-x-5 rounded-b-md border-[#4F46E5] transition-all">
                             <p class="text-xs md:text-sm text-gray-800 text-start">20 Juni 2025 | Yogyakarta</p>
-                            <p class="font-semibold break-words text-start text-[#4F46E5] my-1">Open library talks  creative industry development.......</p>
-                            <p class="text-xs md:text-sm text-gray-800 text-start">In order to save time you have to break down the content strategy for the event or conference you are planning step by step..................</p>
+                            <p class="font-semibold break-words text-start text-[#4F46E5] my-1">Open library talks
+                                creative industry development.......</p>
+                            <p class="text-xs md:text-sm text-gray-800 text-start">In order to save time you have to
+                                break down the content strategy for the event or conference you are planning step by
+                                step..................</p>
                             <div class="flex gap-2 items-center mt-3">
-                                <a href="" class="px-5 py-2 rounded-2xl bg-[#FFB71A] text-white font-semibold text-sm hover:bg-white hover:text-[#FFB71A] hover:outline-[#FFB71A] hover:outline-1 transition-all">
+                                <a href=""
+                                    class="px-5 py-2 rounded-2xl bg-[#FFB71A] text-white font-semibold text-sm hover:bg-white hover:text-[#FFB71A] hover:outline-[#FFB71A] hover:outline-1 transition-all">
                                     Buy Tickets
                                 </a>
-                                <a href="" class="px-5 py-2 rounded-2xl bg-[#3D3D3D] text-white font-semibold text-sm hover:bg-gray-300 hover:text-black transition-all">
+                                <a href=""
+                                    class="px-5 py-2 rounded-2xl bg-[#3D3D3D] text-white font-semibold text-sm hover:bg-gray-300 hover:text-black transition-all">
                                     Details
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div class="flex flex-col justify-evenly items-center rounded-md bg-white max-w-[50vh] group overflow-hidden" data-aos="fade-up" data-aos-delay="100">
+                    <div class="flex flex-col justify-evenly items-center rounded-md bg-white max-w-[50vh] group overflow-hidden"
+                        data-aos="fade-up" data-aos-delay="100">
                         <div class="overflow-hidden">
-                            <img src="/img/landingpage/event.png" alt="" class="rounded-t-md w-[50vh] h-[30vh] object-cover scale-105 overflow-hidden group-hover:scale-100 group-hover:brightness-90 transition-all">
+                            <img src="/img/landingpage/event.png" alt=""
+                                class="rounded-t-md w-[50vh] h-[30vh] object-cover scale-105 overflow-hidden group-hover:scale-100 group-hover:brightness-90 transition-all">
                         </div>
-                        <div class="flex flex-col gap-2 p-5">
+                        <div
+                            class="flex flex-col gap-2 p-5 group-hover:border-b-5 group-hover:border-x-5 rounded-b-md border-[#4F46E5] transition-all">
                             <p class="text-xs md:text-sm text-gray-800 text-start">20 Juni 2025 | Yogyakarta</p>
-                            <p class="font-semibold break-words text-start text-[#4F46E5] my-1 w-full">Open library talks  creative industry development.......</p>
-                            <p class="text-xs md:text-sm text-gray-800 text-start">In order to save time you have to break down the content strategy for the event or conference you are planning step by step..................</p>
+                            <p class="font-semibold break-words text-start text-[#4F46E5] my-1 w-full">Open library
+                                talks creative industry development.......</p>
+                            <p class="text-xs md:text-sm text-gray-800 text-start">In order to save time you have to
+                                break down the content strategy for the event or conference you are planning step by
+                                step..................</p>
                             <div class="flex gap-2 items-center mt-3">
-                                <a href="" class="px-5 py-2 rounded-2xl bg-[#FFB71A] text-white font-semibold text-sm hover:bg-white hover:text-[#FFB71A] hover:outline-[#FFB71A] hover:outline-1 transition-all">
+                                <a href=""
+                                    class="px-5 py-2 rounded-2xl bg-[#FFB71A] text-white font-semibold text-sm hover:bg-white hover:text-[#FFB71A] hover:outline-[#FFB71A] hover:outline-1 transition-all">
                                     Buy Tickets
                                 </a>
-                                <a href="" class="px-5 py-2 rounded-2xl bg-[#3D3D3D] text-white font-semibold text-sm hover:bg-gray-300 hover:text-black transition-all">
+                                <a href=""
+                                    class="px-5 py-2 rounded-2xl bg-[#3D3D3D] text-white font-semibold text-sm hover:bg-gray-300 hover:text-black transition-all">
                                     Details
                                 </a>
                             </div>
--- resources/views/components/landingpage/news.blade.php
+++ resources/views/components/landingpage/news.blade.php
@@ -1,47 +1,133 @@
 <section>
     <div class="w-full bg-white py-10 px-3 relative overflow-hidden">
-        
+
         <!-- Background kiri-->
         <div class="absolute inset-y-0 -left-15 w-1/3">
-            <img src="/img/landingpage/sidebg-mengapa.png" 
-                class="h-full w-full object-cover transform scale-x-[-1]" 
+            <img src="/img/landingpage/sidebg-mengapa.png" class="h-full w-full object-cover transform scale-x-[-1]"
                 alt="">
         </div>
 
         <!-- Background kanan-->
         <div class="absolute inset-y-0 -right-15 w-1/3">
-            <img src="/img/landingpage/sidebg-mengapa.png" 
-                class="h-full w-full object-cover transform" 
-                alt="">
+            <img src="/img/landingpage/sidebg-mengapa.png" class="h-full w-full object-cover transform" alt="">
         </div>
 
         <div class="max-w-7xl mx-auto relative z-10">
             <div class="flex justify-between items-center">
                 <h1 class="font-semibold text-start text-black text-lg md:text-2xl">What News </h1>
-                <a href="#" class="bg-[#3D3D3D] text-white py-2 px-7 rounded-md text-xs md:text-sm hover:bg-white hover:text-[#3D3D3D] hover:outline hover:outline-[#3D3D3D] transition-all">Lainnya ></a>
+                <a href="#"
+                    class="bg-[#3D3D3D] text-white py-2 px-7 rounded-md text-xs md:text-sm hover:bg-white hover:text-[#3D3D3D] hover:outline hover:outline-[#3D3D3D] transition-all">Lainnya
+                    ></a>
             </div>
-            <div class="flex flex-wrap gap-3 md:gap-5 justify-center items-center my-10" data-aos="fade-up">
-                <div class="flex flex-row  justify-center items-center bg-white rounded-md border-1 border-gray-300 group overflow-hidden">
-                    <div class="overflow-hidden">
-                        <img src="/img/landingpage/news.png" alt="" class="max-w-80 max-h-80 object-cover rounded-tl-md rounded-bl-md group-hover:scale-105 group-hover:brightness-90 transition-all">
-                    </div>
-                    <div class="flex flex-col gap-7 px-5 text-start">
-                        <p class="text-xs md:text-sm">July 10 2025</p>
-                        <h1 class="text-sm md:text-base text-black ">Performance and skills in modern arts.......</h1>
-                        <a href="" class="text-xs md:text-sm">More Details</a>
-                    </div>
+            <!-- Wrapper Slider -->
+            <div class="relative" data-aos="fade-up">
+                <div class="flex justify-end gap-3 my-5 md:my-10">
+                    <!-- Tombol Navigasi Kiri -->
+                    <button id="prevBtnNews" class="bg-black/50 p-2 px-3 rounded-full hover:bg-black/70 transition">
+                        <ion-icon name="arrow-back-outline" class="text-white pt-1"></ion-icon>
+                    </button>
+
+                    <!-- Tombol Navigasi Kanan -->
+                    <button id="nextBtnNews" class="bg-black/50 p-2 px-3 rounded-full hover:bg-black/70 transition">
+                        <ion-icon name="arrow-forward-outline" class="text-white pt-1"></ion-icon>
+                    </button>
                 </div>
-                <div class="flex flex-row  justify-center items-center bg-white rounded-md border-1 border-gray-300 group overflow-hidden">
-                    <div class="overflow-hidden">
-                        <img src="/img/landingpage/news.png" alt="" class="max-w-80 max-h-80 object-cover rounded-tl-md rounded-bl-md group-hover:scale-105 group-hover:brightness-90 transition-all">
-                    </div>
-                    <div class="flex flex-col gap-7 px-5 text-start">
-                        <p class="text-xs md:text-sm">July 10 2025</p>
-                        <h1 class="text-sm md:text-base text-black ">Performance and skills in modern arts.......</h1>
-                        <a href="" class="text-xs md:text-sm">More Details</a>
+
+                <!-- Swiper Container -->
+                <div class="swiper mySwiperNews my-10">
+                    <div class="swiper-wrapper">
+
+                        <div class="swiper-slide">
+                            <div
+                                class="flex flex-row justify-center max-w-130 mx-auto items-center bg-white rounded-md border border-gray-300 group overflow-hidden">
+                                <div class="overflow-hidden">
+                                    <img src="/img/landingpage/news.png" alt=""
+                                        class="max-w-80 max-h-80 object-cover rounded-tl-md rounded-bl-md group-hover:scale-105 group-hover:brightness-90 transition-all">
+                                </div>
+                                <div class="flex flex-col gap-7 px-5 text-start">
+                                    <p class="text-xs md:text-sm">July 10 2025</p>
+                                    <h1 class="text-sm md:text-base text-black">Performance and skills in modern
+                                        arts.......</h1>
+                                    <a href="" class="text-xs md:text-sm">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <div
+                                class="flex flex-row justify-center max-w-130 mx-auto  items-center bg-white rounded-md border border-gray-300 group overflow-hidden">
+                                <div class="overflow-hidden">
+                                    <img src="/img/landingpage/news.png" alt=""
+                                        class="max-w-80 max-h-80 object-cover rounded-tl-md rounded-bl-md group-hover:scale-105 group-hover:brightness-90 transition-all">
+                                </div>
+                                <div class="flex flex-col gap-7 px-5 text-start">
+                                    <p class="text-xs md:text-sm">July 10 2025</p>
+                                    <h1 class="text-sm md:text-base text-black">Performance and skills in modern
+                                        arts.......</h1>
+                                    <a href="" class="text-xs md:text-sm">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <div
+                                class="flex flex-row justify-center max-w-130 mx-auto  items-center bg-white rounded-md border border-gray-300 group overflow-hidden">
+                                <div class="overflow-hidden">
+                                    <img src="/img/landingpage/news.png" alt=""
+                                        class="max-w-80 max-h-80 object-cover rounded-tl-md rounded-bl-md group-hover:scale-105 group-hover:brightness-90 transition-all">
+                                </div>
+                                <div class="flex flex-col gap-7 px-5 text-start">
+                                    <p class="text-xs md:text-sm">July 10 2025</p>
+                                    <h1 class="text-sm md:text-base text-black">Performance and skills in modern
+                                        arts.......</h1>
+                                    <a href="" class="text-xs md:text-sm">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <div
+                                class="flex flex-row justify-center max-w-130 mx-auto  items-center bg-white rounded-md border border-gray-300 group overflow-hidden">
+                                <div class="overflow-hidden">
+                                    <img src="/img/landingpage/news.png" alt=""
+                                        class="max-w-80 max-h-80 object-cover rounded-tl-md rounded-bl-md group-hover:scale-105 group-hover:brightness-90 transition-all">
+                                </div>
+                                <div class="flex flex-col gap-7 px-5 text-start">
+                                    <p class="text-xs md:text-sm">July 10 2025</p>
+                                    <h1 class="text-sm md:text-base text-black">Performance and skills in modern
+                                        arts.......</h1>
+                                    <a href="" class="text-xs md:text-sm">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<!-- Swiper JS -->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        new Swiper(".mySwiperNews", {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            slidesPerGroup: 1,
+            loop: true,
+            navigation: {
+                nextEl: "#nextBtnNews",
+                prevEl: "#prevBtnNews",
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1
+                },
+                768: {
+                    slidesPerView: 2
+                }
+            }
+        });
+    })
+</script>
